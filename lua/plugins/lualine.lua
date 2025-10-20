207d81ee134e5252941fbe074076c19ca3259fbb
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-10-20 17:30:40 +0400

    chore: remove redundant configs

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,14 +2,7 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     require("lualine").setup({
-      options = {
-        theme = "dracula",
-        sections = {
-          lualine_a = {
-            file = 1,
-          },
-        },
-      },
+      options = { theme = "dracula" } 
     })
   end,
 }
