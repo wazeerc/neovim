9512815bc09de5b8f13eff29ecd000b5b5bae1c2
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-10-22 21:49:12 +0400

    feat: add vim configs

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,4 +2,13 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.opt.autoindent = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.swapfile = false 
+vim.opt.autoread = true
+vim.opt.mouse = "a"
+vim.opt.clipboard:append("unnamedplus")
+
 vim.g.mapleader = " "
