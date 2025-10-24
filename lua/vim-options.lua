15b80f2c1d9d97dd2f24aa53fedc52c8db499de1
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-10-24 22:16:44 +0400

    style: set habamax colorscheme and make Normal/NormalFloat backgrounds transparent

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,10 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set tabstop=2")
 
+vim.cmd.colorscheme("habamax")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-t>", ":bn<cr>")
