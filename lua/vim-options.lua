5a11f9ed1494641f0b535bc305f9959c42605189
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-10-24 21:46:45 +0400

    style: re order options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,20 +1,20 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-
-vim.opt.autoindent = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.swapfile = false
-vim.opt.autoread = true
-vim.opt.mouse = "a"
-vim.opt.clipboard:append("unnamedplus")
-vim.opt.fillchars = { eob = " " }
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.number = true
+vim.cmd("set softtabstop=2")
+vim.cmd("set tabstop=2")
 
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-t>", ":bn<cr>")
+
+vim.opt.autoindent = true
+vim.opt.autoread = true
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.fillchars = { eob = " " }
+vim.opt.incsearch = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.smartcase = true
+vim.opt.swapfile = false
