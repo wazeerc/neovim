aa3fa9b015e27b928c30f4618a827cb8ebf8a2b3
wazeerc <58399263+wazeerc@users.noreply.github.com>
2025-10-25 11:03:38 +0400

    feat: add nerd font

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,6 +8,7 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.g.mapleader = " "
+vim.g.have_nerd_font = true
 
 vim.keymap.set("n", "<C-t>", ":bn<cr>")
 
